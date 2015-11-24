75f05d56b5afad21b838f8bef6db3dd5651d613c
superouman <philippe.cwik@gmail.com>
2015-11-24 15:36:48 +0100

    Carte de test fonctionnelle

--- map-test/test.js
+++ map-test/test.js
@@ -1,10 +1,12 @@
 function init(){
-  var map = L.map('map').setView([48.505, 2.09], 13);
-  var test = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={pk.eyJ1Ijoic3VwZXJvdW1hbiIsImEiOiJjaWhkZG9jMzEwMDFtdHhtNGdydnFkd2h3In0.OlnqvMNSYGWYvAK9JQs8Zg}', {
-    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-    maxZoom: 18,
-    id: 'superouman.testmap',
-    accessToken: 'pk.eyJ1Ijoic3VwZXJvdW1hbiIsImEiOiJjaWhkZnY5MjUwMDF6djFtOXp4Y2FuMWtxIn0.CYwKPlSVunpIyX1A5sMPfg'
-}).addTo(map);
+	L.mapbox.accessToken = 'pk.eyJ1Ijoic3VwZXJvdW1hbiIsImEiOiJjaWhkZG9jMzEwMDFtdHhtNGdydnFkd2h3In0.OlnqvMNSYGWYvAK9JQs8Zg';
+	// Replace 'mapbox.streets' with your map id.
+	var mapboxTiles = L.tileLayer('https://api.mapbox.com/v4/superouman.o8eed0e9/{z}/{x}/{y}.png?access_token=' + L.mapbox.accessToken, {
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>'
+	});
+
+	var map = L.map('map')
+    	.addLayer(mapboxTiles)
+    	.setView([48.848, 2.334], 11);
 }
 
